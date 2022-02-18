b06611709ab4c156e6add4219a208177c25feeac
okkdev <dev@stehlik.me>
2022-02-18 18:38:57 +0100

    add card types

--- lib/cardian/api/masterduelmeta.ex
+++ lib/cardian/api/masterduelmeta.ex
@@ -41,6 +41,41 @@ defmodule Cardian.Api.Masterduelmeta do
     end
   end
 
+  def get_image(card_id) when is_binary(card_id) do
+    "https://s3.duellinksmeta.com/cards/#{card_id}_w420.webp"
+    |> URI.encode()
+  end
+
+  def get_card_link(card_name) when is_binary(card_name) do
+    ("https://www.masterduelmeta.com/cards/" <> card_name)
+    |> URI.encode()
+  end
+
+  def get_sets_by_id(sets) when is_list(sets) do
+    url =
+      sets
+      |> Enum.join(",")
+      |> then(&"#{@url}/sets?_id[$in]=#{&1}")
+      |> URI.encode()
+
+    Finch.build(:get, url)
+    |> Finch.request(MyFinch)
+    |> handle_response()
+  end
+
+  def get_set_link(url_path) do
+    "https://www.masterduelmeta.com/articles" <> url_path
+  end
+
+  # Legacy set link generation
+  # def get_set_link(set_name) when is_binary(set_name) do
+  #   set_name
+  #   |> String.downcase()
+  #   |> String.replace(" ", "-")
+  #   |> then(&("https://www.masterduelmeta.com/articles/sets/" <> &1))
+  #   |> URI.encode()
+  # end
+
   defp handle_response(resp) do
     case resp do
       {:ok, res} ->
--- lib/cardian/builder.ex
+++ lib/cardian/builder.ex
@@ -1,49 +1,17 @@
 defmodule Cardian.Builder do
   import Nostrum.Struct.Embed
   alias Nostrum.Struct.Embed
+  alias Cardian.Api.Masterduelmeta
 
-  # %{
-  #   "_id" => "60c2b3aaa0e24f2d54a51cec",
-  #   "atk" => 1700,
-  #   "attribute" => "LIGHT",
-  #   "deckTypes" => ["Crusadia", "Cyber Dragon", "Thunder Dragon",
-  #    "Dogmatika Nekroz", "Kaiju"],
-  #   "def" => 1000,
-  #   "description" => "Cannot be Special Summoned. While you control no Set Spells/Traps, neither player can Set Spells/Traps nor activate Spell/Trap Cards that are Set on the field.",
-  #   "konamiID" => "13974207",
-  #   "level" => 4,
-  #   "linkArrows" => [],
-  #   "monsterType" => ["Effect"],
-  #   "name" => "Denko Sekka",
-  #   "obtain" => [
-  #     %{
-  #       "amount" => 1,
-  #       "source" => %{
-  #         "_id" => "61eaddc798a43480f06172db",
-  #         "name" => "Master Pack"
-  #       },
-  #       "type" => "sets"
-  #     }
-  #   ],
-  #   "ocgRelease" => "2014-07-19T00:00:00.000Z",
-  #   "popRank" => 893,
-  #   "race" => "Thunder",
-  #   "rarity" => "SR",
-  #   "release" => "2022-01-19T06:00:00.000Z",
-  #   "tcgRelease" => "2014-11-06T00:00:00.000Z",
-  #   "type" => "Monster"
-  # }
+  # TODO: Rewrite generically, create datatypes
 
   def build_card_message(card) do
-    embed =
-      %Embed{}
-      |> put_title(card["name"])
-
-    %{
-      embeds: [
-        embed
-      ]
-    }
+    case card["type"] do
+      "Monster" -> build_monster_message(card)
+      "Spell" -> build_spelltrap_message(card)
+      "Trap" -> build_spelltrap_message(card)
+      _ -> build_user_message("Card type \"#{card["type"]}\" not supported yet :pensive:")
+    end
   end
 
   def build_user_message(body) do
@@ -62,5 +30,169 @@ defmodule Cardian.Builder do
     %{
       choices: choices
     }
+  end
+
+  defp build_spelltrap_message(card) do
+    embed =
+      %Embed{}
+      |> put_title(card["name"])
+      |> put_url(Masterduelmeta.get_card_link(card["name"]))
+      |> put_thumbnail(Masterduelmeta.get_image(card["_id"]))
+      |> put_color(if card["type"] == "Spell", do: 1_941_108, else: 12_343_940)
+      |> put_field("Type", card["race"], true)
+      |> put_field("Effect", card["description"])
+      |> put_field("Rarity", "**#{card["rarity"]}**", true)
+      |> put_field(
+        "Status",
+        if(card["banStatus"], do: card["banStatus"], else: "Unlimited"),
+        true
+      )
+      |> put_field("Obtain", put_sets(card["obtain"]), true)
+
+    %{
+      embeds: [
+        embed
+      ]
+    }
+  end
+
+  defp build_monster_message(card) do
+    monster_type = get_monster_type(card["monsterType"])
+
+    embed =
+      %Embed{}
+      |> put_title(card["name"])
+      |> put_url(Masterduelmeta.get_card_link(card["name"]))
+      |> put_thumbnail(Masterduelmeta.get_image(card["_id"]))
+      |> put_color(get_monster_color(monster_type))
+      |> put_field("Attribute", card["attribute"], true)
+      |> put_monster_fields(card, monster_type)
+      |> put_field("Rarity", "**#{card["rarity"]}**", true)
+      |> put_field(
+        "Status",
+        if(card["banStatus"], do: card["banStatus"], else: "Unlimited"),
+        true
+      )
+      |> put_field("Obtain", put_sets(card["obtain"]), true)
+
+    %{
+      embeds: [
+        embed
+      ]
+    }
+  end
+
+  defp put_monster_fields(embed, card, :link) do
+    embed
+    |> put_field("Link Rating", card["linkRating"], true)
+    |> put_field("Type", Enum.join([card["race"] | card["monsterType"]], " / "), true)
+    |> put_field("Monster Effect", card["description"])
+    |> put_field("ATK", card["atk"], true)
+    |> put_field("Link Arrows", put_link_arrows(card["linkArrows"]), true)
+    |> put_field("\u200b", "\u200b", true)
+  end
+
+  defp put_monster_fields(embed, card, :pendulum) do
+    embed
+    |> put_field("Level", card["level"], true)
+    |> put_field("Type", Enum.join([card["race"] | card["monsterType"]], " / "), true)
+    |> put_field("Description", card["description"])
+    |> put_field("ATK", card["atk"], true)
+    |> put_field("DEF", card["def"], true)
+    |> put_field("⬅️Scale➡️", card["scale"], true)
+  end
+
+  defp put_monster_fields(embed, card, type) do
+    embed
+    |> put_field(if(type == :xyz, do: "Rank", else: "Level"), card["level"], true)
+    |> put_field("Type", Enum.join([card["race"] | card["monsterType"]], " / "), true)
+    |> put_field(
+      if(type == :normal, do: "Description", else: "Monster Effect"),
+      card["description"]
+    )
+    |> put_field("ATK", card["atk"], true)
+    |> put_field("DEF", card["def"], true)
+    |> put_field("\u200b", "\u200b", true)
+  end
+
+  defp put_link_arrows(arrows) do
+    mapping = %{
+      "Top-Left" => "↖️",
+      "Top" => "⬆️",
+      "Top-Right" => "↗️",
+      "Left" => "⬅️",
+      "Right" => "➡️",
+      "Bottom-Left" => "↙️",
+      "Bottom" => "⬇️",
+      "Bottom-Right" => "↘️"
+    }
+
+    arrows
+    |> Enum.map(&mapping[&1])
+    |> Enum.join()
+  end
+
+  defp put_sets(sets) when length(sets) > 0 do
+    sets
+    |> Enum.map(& &1["source"]["_id"])
+    |> Masterduelmeta.get_sets_by_id()
+    |> Enum.map(&put_set_link/1)
+    |> Enum.join()
+  end
+
+  defp put_sets(_) do
+    "No Packs"
+  end
+
+  defp put_set_link(set) do
+    case set["linkedArticle"] do
+      %{"url" => url} ->
+        "[#{set["name"]}](#{Masterduelmeta.get_set_link(url)})\n"
+
+      _ ->
+        "#{set["name"]}\n"
+    end
+  end
+
+  defp get_monster_type(type) do
+    case hd(type) do
+      "Synchro" -> :synchro
+      "Effect" -> :effect
+      "Normal" -> :normal
+      "Xyz" -> :xyz
+      "Fusion" -> :fusion
+      "Pendulum" -> :pendulum
+      "Link" -> :link
+      "Ritual" -> :ritual
+      _ -> nil
+    end
+  end
+
+  defp get_monster_color(:synchro) do
+    15_132_390
+  end
+
+  defp get_monster_color(:fusion) do
+    10_520_247
+  end
+
+  defp get_monster_color(:ritual) do
+    10_335_692
+  end
+
+  defp get_monster_color(:link) do
+    139
+  end
+
+  defp get_monster_color(:normal) do
+    16_639_626
+  end
+
+  defp get_monster_color(:xyz) do
+    0
+  end
+
+  defp get_monster_color(_) do
+    13_665_078
   end
 end
