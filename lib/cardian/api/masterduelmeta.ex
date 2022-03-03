2c03b4f0221ff19f6a6dab7bc7baea329c21cdf7
okkdev <dev@stehlik.me>
2022-03-03 19:42:49 +0100

    add icons

--- lib/cardian/api/masterduelmeta.ex
+++ lib/cardian/api/masterduelmeta.ex
@@ -3,6 +3,49 @@ defmodule Cardian.Api.Masterduelmeta do
 
   @url "https://www.masterduelmeta.com/api/v1"
 
+  @rarity_mapping %{
+    "N" => "<:normal:948990033321414678>",
+    "R" => "<:rare:948990141786095667>",
+    "SR" => "<:superrare:948990076111712356>",
+    "UR" => "<:ultrarare:948990098920333332>"
+  }
+
+  @status_mapping %{
+    "Limited 2" => "<:semilimited:948990692842156043>",
+    "Limited 1" => "<:limited:948990713272602695>",
+    "Forbidden" => "<:forbidden:948990744373387386>"
+  }
+
+  @arrow_mapping %{
+    "Top-Left" => "↖️",
+    "Top" => "⬆️",
+    "Top-Right" => "↗️",
+    "Left" => "⬅️",
+    "Right" => "➡️",
+    "Bottom-Left" => "↙️",
+    "Bottom" => "⬇️",
+    "Bottom-Right" => "↘️"
+  }
+
+  @attribute_mapping %{
+    "DARK" => "<:DARK:948992874400346152>",
+    "DIVINE" => "<:DIVINE:948992874089947136>",
+    "EARTH" => "<:EARTH:948992874442285096>",
+    "FIRE" => "<:FIRE:948992874375176212>",
+    "LIGHT" => "<:LIGHT:948992874396151879>",
+    "WATER" => "<:WATER:948992874136096768>",
+    "WIND" => "<:WIND:948992874123505775>"
+  }
+
+  @card_type_mapping %{
+    "Quick-Play" => "<:quickplay:948992874366771240>",
+    "Ritual" => "<:ritual:948992874580680786>",
+    "Field" => "<:field:948992874169630750>",
+    "Equip" => "<:equip:948992874039623741>",
+    "Continuous" => "<:continuous:948992874421305385>",
+    "Counter" => "<:counter:948992874400321617>"
+  }
+
   def get_all_cards() do
     pages = ceil(get_card_amount() / 3000)
 
@@ -80,9 +123,7 @@ defmodule Cardian.Api.Masterduelmeta do
     |> URI.encode()
   end
 
-  defp get_set_link(_) do
-    nil
-  end
+  defp get_set_link(_), do: nil
 
   defp handle_response(resp) do
     case resp do
@@ -100,10 +141,10 @@ defmodule Cardian.Api.Masterduelmeta do
     %Card{
       id: resp["_id"],
       type: get_card_type(resp["type"]),
-      race: resp["race"],
+      race: parse_card_race(resp["race"]),
       monster_type: get_monster_type(resp["monsterType"]),
       monster_types: resp["monsterType"],
-      attribute: resp["attribute"],
+      attribute: @attribute_mapping[resp["attribute"]],
       level: resp["level"] || resp["linkRating"],
       name: resp["name"],
       description: description,
@@ -112,8 +153,8 @@ defmodule Cardian.Api.Masterduelmeta do
       def: resp["def"],
       scale: resp["scale"],
       arrows: parse_link_arrows(resp["linkArrows"]),
-      status: resp["banStatus"] || "Unlimited",
-      rarity: resp["rarity"],
+      status: @status_mapping[resp["banStatus"]] || "Unlimited",
+      rarity: @rarity_mapping[resp["rarity"]],
       image_url: get_image(resp["_id"]),
       url: get_card_link(resp["name"]),
       sets: Enum.map(resp["obtain"], & &1["source"]["_id"])
@@ -138,32 +179,23 @@ defmodule Cardian.Api.Masterduelmeta do
     }
   end
 
-  defp parse_effects(description) do
-    {nil, description}
-  end
+  defp parse_effects(description), do: {nil, description}
 
   defp parse_link_arrows(arrows) when is_list(arrows) and length(arrows) > 0 do
-    mapping = %{
-      "Top-Left" => "↖️",
-      "Top" => "⬆️",
-      "Top-Right" => "↗️",
-      "Left" => "⬅️",
-      "Right" => "➡️",
-      "Bottom-Left" => "↙️",
-      "Bottom" => "⬇️",
-      "Bottom-Right" => "↘️"
-    }
-
     arrows
-    |> Enum.map(&mapping[&1])
+    |> Enum.map(&@arrow_mapping[&1])
     |> Enum.join()
     # Add invisible character to force mobile to show small emojis
     |> then(&(&1 <> "‎"))
   end
 
-  defp parse_link_arrows(_) do
-    nil
+  defp parse_link_arrows(_), do: nil
+
+  defp parse_card_race(race) when is_map_key(@card_type_mapping, race) do
+    @card_type_mapping[race]
   end
+
+  defp parse_card_race(race), do: race
 
   defp get_card_type(type) do
     case type do
@@ -189,7 +221,5 @@ defmodule Cardian.Api.Masterduelmeta do
     end
   end
 
-  defp get_monster_type(_) do
-    nil
-  end
+  defp get_monster_type(_), do: nil
 end
