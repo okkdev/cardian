97792ca16f6c164c7f89d5fc3865a8a47c054436
okkdev <dev@stehlik.me>
2022-03-19 22:52:01 +0100

    limit intents

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,7 +1,10 @@
 import Config
 
 config :nostrum,
-  token: System.fetch_env!("CARDIAN_TOKEN")
+  token: System.fetch_env!("CARDIAN_TOKEN"),
+  gateway_intents: [
+    :guilds
+  ]
 
 config :cardian,
   update_interval: String.to_integer(System.get_env("CARDIAN_UPDATE_INTERVAL", "120"))
