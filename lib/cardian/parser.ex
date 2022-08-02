83fb6c615833041fbf6ae4f60524c6bc858246fb
okkdev <dev@stehlik.me>
2022-08-02 20:53:36 +0200

    🚑 ignore emojis and users

--- lib/cardian/parser.ex
+++ lib/cardian/parser.ex
@@ -3,6 +3,13 @@ defmodule Cardian.Parser do
 
   card_name =
     ignore(string("<"))
+    |> lookahead_not(
+      choice([
+        utf8_string([?:, ?@, ?#], 1),
+        string("t:"),
+        string("a:")
+      ])
+    )
     |> utf8_string([not: ?>], min: 1, max: 30)
     |> ignore(string(">"))
 
