2a8548e399b0fddde6207782ac5f30c2fa75cced
okkdev <dev@stehlik.me>
2023-07-12 09:50:54 +0200

    update deps

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Cardian.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nostrum, "~> 0.6.1"},
+      {:nostrum, "~> 0.8.0"},
       {:req, "~> 0.3.0"},
       {:nimble_parsec, "~> 1.2"},
       {:sentry, "~> 8.0"},
