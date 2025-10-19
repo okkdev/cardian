d90a180ee282c1423bee7d2656d08adfedfa2412
okkdev <dev@stehlik.me>
2025-10-20 00:04:26 +0200

    fix: point wording

--- lib/cardian/builder.ex
+++ lib/cardian/builder.ex
@@ -85,7 +85,9 @@ defmodule Cardian.Builder do
     }
   end
 
-  defp put_format_footer(embed, :paper), do: put_footer(embed, "Format: Paper (TCG, OCG, Genesys)")
+  defp put_format_footer(embed, :paper),
+    do: put_footer(embed, "Format: Paper (TCG, OCG, Genesys)")
+
   defp put_format_footer(embed, :md), do: put_footer(embed, "Format: Master Duel")
   defp put_format_footer(embed, :dl), do: put_footer(embed, "Format: Duel Links")
   defp put_format_footer(embed, :sd), do: put_footer(embed, "Format: Speed Duel")
@@ -226,10 +228,16 @@ defmodule Cardian.Builder do
   defp put_card_rarity(_), do: ""
 
   defp put_card_status(%Card{} = card, :paper) do
+    points =
+      case card.genesys_points do
+        1 -> "1 Point"
+        n -> "#{n} Points"
+      end
+
     """
     **TCG**: #{status_icon(card.status_tcg)}
     **OCG**: #{status_icon(card.status_ocg)}
-    **Genesys**: #{card.genesys_points} Points
+    **Genesys**: #{points}
     """
   end
 
